4b92de231fbf7d5b793abf67649d4fd6512ebdfb
兰聪 <515@qq.com>
2025-12-30 09:55:52 +0800

    feat: add swipe refresh and cache management

--- android/app/src/main/java/com/jsi/app/MainActivity.java
+++ android/app/src/main/java/com/jsi/app/MainActivity.java
@@ -2,20 +2,24 @@ package com.jsi.app;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.webkit.WebResourceRequest;
-import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import androidx.annotation.NonNull;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+    private SwipeRefreshLayout swipeRefreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         WebView webView = getBridge().getWebView();
-        android.webkit.WebSettings settings = webView.getSettings();
+        WebSettings settings = webView.getSettings();
         
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
@@ -23,13 +27,13 @@ public class MainActivity extends BridgeActivity {
         settings.setAppCacheEnabled(true);
         settings.setAllowFileAccess(true);
         settings.setAllowContentAccess(true);
-        settings.setMixedContentMode(android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
         settings.setLoadWithOverviewMode(true);
         settings.setUseWideViewPort(true);
         settings.setSupportZoom(false);
         settings.setBuiltInZoomControls(false);
         settings.setDisplayZoomControls(false);
-        settings.setCacheMode(android.webkit.WebSettings.LOAD_DEFAULT);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
         settings.setMediaPlaybackRequiresUserGesture(false);
         
         webView.setWebViewClient(new WebViewClient() {
@@ -50,7 +54,42 @@ public class MainActivity extends BridgeActivity {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
+                if (swipeRefreshLayout != null) {
+                    swipeRefreshLayout.setRefreshing(false);
+                }
             }
         });
+        
+        setupSwipeRefresh(webView);
+    }
+    
+    private void setupSwipeRefresh(WebView webView) {
+        try {
+            swipeRefreshLayout = new SwipeRefreshLayout(this);
+            swipeRefreshLayout.addView(webView);
+            
+            swipeRefreshLayout.setColorSchemeResources(
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light
+            );
+            
+            swipeRefreshLayout.setOnRefreshListener(() -> {
+                webView.clearCache(true);
+                webView.reload();
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        WebView webView = getBridge().getWebView();
+        if (webView != null) {
+            webView.clearCache(false);
+        }
     }
 }
